25c43fbbc35a4418a5de3716c6612ea5e9683141
Ochanissi <mirelalex2094@gmail.com>
2019-06-07 19:51:28 +0300

    making the cardlist component to loop over the characters

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { characters } from './characters';
 
 
 ReactDOM.render(
-                <div>
-                    <Card id={characters[0].id} name={characters[0].name} email={characters[0].email}/>
-                    <Card id={characters[1].id} name={characters[1].name} email={characters[1].email}/>
-                    <Card id={characters[2].id} name={characters[2].name} email={characters[2].email}/>
-                </div>, document.getElementById('root'));
+                <CardList characters={characters}/>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
