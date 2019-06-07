fdafa20cc021c27dee1efe09b7bbc171349be322
Ochanissi <mirelalex2094@gmail.com>
2019-06-07 19:34:45 +0300

    building the card component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { characters } from './characters';
 
 
-ReactDOM.render(<Hello greeting={'Hello' + 'Reach ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                    <Card id={characters[0].id} name={characters[0].name} email={characters[0].email}/>
+                    <Card id={characters[1].id} name={characters[1].name} email={characters[1].email}/>
+                    <Card id={characters[2].id} name={characters[2].name} email={characters[2].email}/>
+                </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
