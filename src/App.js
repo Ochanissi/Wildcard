dccac4c5bb57efba5ba493dbf20ab6e444d9f6a4
Ochanissi <mirelalex2094@gmail.com>
2019-06-08 17:16:12 +0300

    adding the searchbox with the functionality

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox'
+import { characters } from './characters';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            characters: characters,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        // console.log(event.target.value);
+        // console.log(filteredCharacters);
+    }
+    render() {
+        const filteredCharacters = this.state.characters.filter(characters => {
+            return characters.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>WildCard</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList characters={filteredCharacters}/>
+            </div>
+        );
+    }
+}
+
+export default App;
