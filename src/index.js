c8f275d1d130112cd4ac9d6a497be97fc22765aa
Ochanissi <mirelalex2094@gmail.com>
2019-06-08 19:22:04 +0300

    final touches, cleaning the code, structuring the app to folders, building the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import App from './App';
+import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
