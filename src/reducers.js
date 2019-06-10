0ab6e8f39a3f1f098d86673be85f1d53de3a9b06
Ochanissi <mirelalex2094@gmail.com>
2019-06-10 16:24:35 +0300

    adding redux store and provider

--- src/reducers.js
+++ src/reducers.js
@@ -6,9 +6,9 @@ const initialState = {
 
 export const searchCharacters = (state=initialState, action={}) => {
     switch(action.type) {
-        case: CHANGE_SEARCHFIELD:
-            return Object.assign({}, state, searchField:action.payload);
-            return { ..state, searchField:action.payload}; // Object destructuring / Object spread operator
+        case CHANGE_SEARCHFIELD:
+            return Object.assign({}, state, { searchField:action.payload });
+            // return { ..state, searchField:action.payload}; // Object destructuring / Object spread operator
         default:
             return state;
     }
