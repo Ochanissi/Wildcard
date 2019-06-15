77575883dbded8efabbbb0c884deb93297c81e80
Ochanissi <mirelalex2094@gmail.com>
2019-06-15 13:59:53 +0300

    optimizing the app via lighthouse

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, email, id }) => {
     // const { name, email, id } = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
-            <img alt= 'card' src={`https://robohash.org/${id}?200x200`}/>
+            <img alt= 'card' src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
